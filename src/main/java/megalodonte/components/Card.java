c0907ec617ec02f1f6f9efde2cadc0dfedfdc6a4
eliezer-software-enginner <eliezerassuncaocustodio@gmail.com>
2026-01-03 20:48:33 -0300

    impedindo crescimento do card. Ele agora só tem o tamanho do filho

--- src/main/java/megalodonte/components/Card.java
+++ src/main/java/megalodonte/components/Card.java
@@ -1,5 +1,6 @@
 package megalodonte.components;
 
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import megalodonte.props.CardProps;
 import megalodonte.styles.CardStyler;
@@ -12,18 +13,33 @@ public class Card extends Component {
         super(new StackPane(), new CardProps(), new CardStyler());
 
         this.container = (StackPane) node;
+
+        impedirCrescimentoAutomaticoDoFilho();
+
         this.container.getChildren().add(content.getNode());
+    }
+
+    private void impedirCrescimentoAutomaticoDoFilho() {
+        //impedir o crescimento automático do Region.
+        this.container.setMaxSize(
+                Region.USE_PREF_SIZE,
+                Region.USE_PREF_SIZE
+        );
     }
 
     public Card(Component content, CardProps props) {
         super(new StackPane(), props, new CardStyler());
         this.container = (StackPane) node;
+        impedirCrescimentoAutomaticoDoFilho();
+
         this.container.getChildren().add(content.getNode());
     }
 
     public Card(Component content, CardProps props, CardStyler styler) {
         super(new StackPane(), props, styler);
         this.container = (StackPane) node;
+        impedirCrescimentoAutomaticoDoFilho();
+
         this.container.getChildren().add(content.getNode());
     }
 }
