b1502ce596700fc56a96ef0fccb05e3bf34f8b6b
Eliezer o grande dev <eliezerassuncaocustodio@gmail.com>
2026-01-13 10:34:11 -0300

    Fix Image component NullPointerException on null/empty state values

--- src/main/java/megalodonte/components/Image.java
+++ src/main/java/megalodonte/components/Image.java
@@ -20,14 +20,36 @@ public class Image extends Component {
     public Image(ReadableState<String> imgSourceState) {
         super(new javafx.scene.image.ImageView());
         this.imgview = (javafx.scene.image.ImageView) this.node;
+        
+        String initialValue = imgSourceState.get();
+        if (initialValue != null && !initialValue.isEmpty()) {
+            imgview.setImage(new javafx.scene.image.Image(initialValue));
+        }
 
-        imgSourceState.subscribe(s -> imgview.setImage(new javafx.scene.image.Image(s)));
+        imgSourceState.subscribe(s -> {
+            if (s != null && !s.isEmpty()) {
+                imgview.setImage(new javafx.scene.image.Image(s));
+            } else {
+                imgview.setImage(null);
+            }
+        });
     }
 
     public Image(ReadableState<String> imgSourceState, ImageProps props) {
         super(new javafx.scene.image.ImageView(), props);
         this.imgview = (javafx.scene.image.ImageView) this.node;
+        
+        String initialValue = imgSourceState.get();
+        if (initialValue != null && !initialValue.isEmpty()) {
+            imgview.setImage(new javafx.scene.image.Image(initialValue));
+        }
 
-        imgSourceState.subscribe(s -> imgview.setImage(new javafx.scene.image.Image(s)));
+        imgSourceState.subscribe(s -> {
+            if (s != null && !s.isEmpty()) {
+                imgview.setImage(new javafx.scene.image.Image(s));
+            } else {
+                imgview.setImage(null);
+            }
+        });
     }
 }
