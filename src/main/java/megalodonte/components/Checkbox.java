9afb4b7c8a9ab1cbe403d9274ee44689d5d6a8de
eliezer-software-enginner <eliezerassuncaocustodio@gmail.com>
2026-04-01 07:39:26 -0300

    created checkbox

---
+++ src/main/java/megalodonte/components/Checkbox.java
@@ -0,0 +1,29 @@
+package megalodonte.components;
+
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import megalodonte.ReadableState;
+import megalodonte.State;
+import megalodonte.base.components.Component;
+import megalodonte.props.ButtonProps;
+import megalodonte.props.SelectProps;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+
+public class Checkbox<T> extends Component  {
+
+    private final CheckBox javaFxCheckbox;
+    private BiPredicate<T, T> itemComparator = (a, b) -> a == b;
+
+    public Checkbox(String label,ReadableState<Boolean> state) {
+        super(new CheckBox(label));
+        this.javaFxCheckbox = (CheckBox) this.node;
+
+        state.subscribe((stateValue)-> javaFxCheckbox.setSelected(stateValue));
+    }
+}
+
